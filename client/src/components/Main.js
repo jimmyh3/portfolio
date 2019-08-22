0ce698b9828b38bd96a4c5c25c29d5758abb33c5
jimmyh3 <jimmyhe3452@gmail.com>
2019-08-22 14:44:31 -0700

    Remade and renamed css classes to use css grid layout.

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactBootstrap from 'react-bootstrap';
 import logo from '../logo.svg'
 import '../index.css';
 import profile_img from '../resources/MyProfilePicture.jpg';
@@ -49,66 +50,87 @@ class Main extends React.Component {
 
     render() {
         const jsx = 
-        <div className="main">
-            {/* Add everything here first and then worry about design later. */}
-            <div className="profile-image-container">
-                <img src={profile_img} alt="Profile Image"></img>
-            </div>
-            <p className="profile-description">
-                Aspiring web developer with a degree in computer science.
-                Experienced in object oriented programming, back end development, testing and debugging.
-                Successful working in teams and independently. Love to learn and apply knowledge.
-            </p>
-
-            <div className="navtab-container">
-
-                <div className="navtab-buttons">
-                    <button 
-                        style={{
-                            backgroundColor: this.state.navtab_buttons.project_bgColor,
-                            color: this.state.navtab_buttons.project_color
-                        }} 
-                        onClick={() => this.handleClickButtonPanel(0, '#f44336')}
-                    >
-                    Projects
-                    </button>
-                    <button
-                        style={{
-                            backgroundColor: this.state.navtab_buttons.aboutMe_bgColor,
-                            color: this.state.navtab_buttons.aboutMe_color
-                        }}
-                        onClick={() => this.handleClickButtonPanel(1, '#4caf50')}
-                    >
-                    About Me
-                    </button>
-                </div>
-                
-                <div 
-                    className="navtab-panel"
-                    style={{
-                        display: this.state.navtab_panels.project_display,
-                        backgroundColor : this.state.navtab_panels.project_bgColor
-                    }}
-                >Projects
-                    <div className="project-card">
-
-                        <img src={logo} alt="Project Image"></img>
-                        <hr />
-                        <div className="project-card-container">
-                            <h4>Project 1</h4>
-                            <p>Project description here...</p>
+        <div>
+            <div className="layout">
+                <header className="grid-one profile-heading">
+                    {/* Add everything here first and then worry about design later. */}
+                    <img src={profile_img} alt="Profile Image"></img>
+                    <h2>Full Stack Developer</h2>
+                    <p>
+                        Aspiring developer with a degree in computer science.
+                        Experienced in object oriented programming, back end development, testing and debugging.
+                        Successful working in teams and independently. Love to learn and apply knowledge.
+                    </p>
+                </header>
+                <main className="grid-two">
+                    {/* */}
+                    <section className="profile-info">
+                        <div className="navtab-buttons">
+                            <button 
+                                style={{
+                                    backgroundColor: this.state.navtab_buttons.project_bgColor,
+                                    color: this.state.navtab_buttons.project_color
+                                }} 
+                                onClick={() => this.handleClickButtonPanel(0, '#f44336')}
+                            >
+                            Projects
+                            </button>
+                            <button
+                                style={{
+                                    backgroundColor: this.state.navtab_buttons.aboutMe_bgColor,
+                                    color: this.state.navtab_buttons.aboutMe_color
+                                }}
+                                onClick={() => this.handleClickButtonPanel(1, '#4caf50')}
+                            >
+                            About Me
+                            </button>
                         </div>
-                    </div>
-                </div>
-                <div 
-                    className="navtab-panel"
-                    style={{
-                        display: this.state.navtab_panels.aboutMe_display,
-                        backgroundColor : this.state.navtab_panels.aboutMe_bgColor
-                    }}
-                > About Me Panels
-                </div>
-                
+
+                        <div 
+                            className="navtab-panel"
+                            style={{
+                                display: this.state.navtab_panels.project_display,
+                                backgroundColor : this.state.navtab_panels.project_bgColor
+                            }}
+                        >
+                            <div className="project-card">
+
+                                <img src={logo} alt="Project Image"></img>
+                                <hr />
+                                <div className="project-card-container">
+                                    <h4>Project 1</h4>
+                                    <p>Project description here...</p>
+                                </div>
+                            </div>
+                            <div className="project-card">
+
+                                <img src={logo} alt="Project Image"></img>
+                                <hr />
+                                <div className="project-card-container">
+                                    <h4>Project 1</h4>
+                                    <p>Project description here...</p>
+                                </div>
+                            </div>
+                            <div className="project-card">
+
+                                <img src={logo} alt="Project Image"></img>
+                                <hr />
+                                <div className="project-card-container">
+                                    <h4>Project 1</h4>
+                                    <p>Project description here...</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div 
+                            className="navtab-panel"
+                            style={{
+                                display: this.state.navtab_panels.aboutMe_display,
+                                backgroundColor : this.state.navtab_panels.aboutMe_bgColor
+                            }}
+                        > About Me Panels
+                        </div>
+                    </section>
+                </main>
             </div>
         </div>
 
