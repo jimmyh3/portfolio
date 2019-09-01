4b91e69cd3ca2113721a2cf0fa352e7fb1cfc309
jimmyh3 <jimmyhe3452@gmail.com>
2019-09-01 15:45:59 -0700

    Refactored UI to have top navigation, different font, spacing ... Changed button sizing to max-content instead of 100%. Changed font to Calibri. Added space between project image and project description in project cards. Removed profile picture.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../index.css';
-import profile_img from '../resources/MyProfilePicture.jpg';
+//import profile_img from '../resources/MyProfilePicture.jpg';
 import ProjectTab from './main/ProjectTab';
 import AboutMeTab from './main/AboutMeTab';
 
@@ -9,6 +9,8 @@ class Main extends React.Component {
     constructor(props) {
         super(props);
         this.refTopOfPage = React.createRef();
+        this.refProjectSection = React.createRef();
+        this.refContactSection = React.createRef();
         this.state = {
             showProjectTab: true,
             showAboutMeTab: false
@@ -37,6 +39,7 @@ class Main extends React.Component {
             activePanel = <AboutMeTab content={{project_display: "block"}}/>;
         }
 
+        /*
         const panelBtnColor = (isActive, activeColor, inactiveColor) => {
             const style = {};
             if (isActive) {
@@ -46,13 +49,19 @@ class Main extends React.Component {
             }
             return style;
         }
-
+        */
+       
         const jsx = 
         <div>
             <div className="layout layout-rwd">
+                <nav className="navbar">
+                    <ul>
+                        <li><a href="#project-section">Contact</a></li>
+                        <li><a href="#contact-section">Project</a></li>
+                    </ul>
+                </nav>
                 <header className="profile-heading" ref={this.refTopOfPage}>
-                    {/* Add everything here first and then worry about design later. */}
-                    <img src={profile_img} alt="Profile"></img>
+                    <h1>Jimmy He</h1>
                     <h2>Full Stack Developer</h2>
                     <p>
                         Aspiring developer with a degree in computer science.
@@ -61,8 +70,10 @@ class Main extends React.Component {
                     </p>
                 </header>
                 <main>
-                    <section className="profile-info">
-                        <div className="navtab-buttons">
+                    <section>
+                        { /*
+                        <div id="project-section" className="navtab-buttons">
+                            
                             <button
                                 id="project-tab"
                                 style={panelBtnColor(this.state.showProjectTab, "lightgray", "darkgray")}
@@ -70,7 +81,7 @@ class Main extends React.Component {
                             >
                             Projects
                             </button>
-
+                            
                             <button
                                 id="aboutme-tab"
                                 style={panelBtnColor(this.state.showAboutMeTab, "lightgray", "darkgray")}
@@ -78,12 +89,19 @@ class Main extends React.Component {
                             >
                             About Me
                             </button>
+                            
                         </div>
-                        
+                        */}
+                        <h2>Projects</h2>
                         {activePanel}
 
                     </section>
                 </main>
+                <footer>
+                    <div id="contact-section">
+
+                    </div>
+                </footer>
             </div>
         </div>
 
