c39678761f132b0499e0ea87c7cfc64b46583f3a
jimmyh3 <jimmyhe3452@gmail.com>
2019-09-08 10:24:15 -0700

    Added project cards skill listing, patched and added project links. Skill listings have been added as inline items with a blue border. Added github button links to the project cards. Patched the project cards link to be left aligned (don't know why display: inline-block with zero margins and padding did not work - used float: left instead).

--- src/components/main/ProjectTab.js
+++ src/components/main/ProjectTab.js
@@ -25,24 +25,47 @@ class ProjectTab extends React.Component {
             <div className="project-card">
                 <img src={teamIChessIcon} alt="Project"></img>
                 <div className="project-card-text">
-                    <h4>Team-I Chess</h4>
+                    <h3>Team-I Chess</h3>
                     <p>
                     Team-I-Chess is an online two player chess game with live chat enabled. 
                     Users may register accounts, create chess games, chat in-game and in lobby.
                     </p>
+                    <ul className="project-card-skills">
+                        <li>Nodejs</li>
+                        <li>MySQL</li>
+                        <li>Bootstrap</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>RESTful</li>
+                    </ul>
                 </div>
-                <a href="http://team-i-chess.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="project-card-link">View Demo</a>
+                <div className="project-card-links">
+                    <a href="http://team-i-chess.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="project-card-link">View Demo</a>
+                    <a href="https://github.com/jimmyh3/team-i-chess" target="_blank" rel="noopener noreferrer" className="project-card-link">View Source Code</a>
+                </div>
             </div>
             <div className="project-card">
                 <img src={myplaceIcon} alt="Project"></img>
                 <div className="project-card-text">
-                    <h4>MyPlace</h4>
+                    <h3>myPlace</h3>
                     <p>
                     myPlace is an apartment rental site tailored toward San Francisco State University students. 
                     Students may register and apply to listings.
                     </p>
+                    <ul className="project-card-skills">
+                        <li>PHP</li>
+                        <li>MySQL</li>
+                        <li>Bootstrap</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                    </ul>
                 </div>
-                <a href="https://myplace-sfsu.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="project-card-link">View Demo</a>
+                <div className="project-card-links">
+                    <a href="https://myplace-sfsu.herokuapp.com/" target="_blank" rel="noopener noreferrer">View Demo</a>
+                    <a href="https://github.com/jimmyh3/myplace" target="_blank" rel="noopener noreferrer">View Source Code</a>                   
+                </div>
             </div>
         </div>
 
