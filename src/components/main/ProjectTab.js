5991a61cebdd7fe08024f76b8e16a0eccd88f31f
jimmyh3 <jimmyhe3452@gmail.com>
2019-09-15 22:30:52 -0700

    Switched ordering of projects to myPlace first.

--- src/components/main/ProjectTab.js
+++ src/components/main/ProjectTab.js
@@ -23,6 +23,28 @@ class ProjectTab extends React.Component {
             }}
         >
             <div className="project-card">
+                <img src={myplaceIcon} alt="Project"></img>
+                <div className="project-card-text">
+                    <h3>myPlace</h3>
+                    <p>
+                    myPlace is an apartment rental site tailored toward San Francisco State University students. 
+                    Students may register and apply to listings.
+                    </p>
+                    <ul className="project-card-skills">
+                        <li>PHP</li>
+                        <li>MySQL</li>
+                        <li>Bootstrap</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                    </ul>
+                </div>
+                <div className="project-card-links">
+                    <a href="https://myplace-sfsu.herokuapp.com/" target="_blank" rel="noopener noreferrer">View Demo</a>
+                    <a href="https://github.com/jimmyh3/myplace" target="_blank" rel="noopener noreferrer">View Source Code</a>                   
+                </div>
+            </div>
+            <div className="project-card">
                 <img src={teamIChessIcon} alt="Project"></img>
                 <div className="project-card-text">
                     <h3>Team-I Chess</h3>
@@ -43,28 +65,6 @@ class ProjectTab extends React.Component {
                 <div className="project-card-links">
                     <a href="http://team-i-chess.herokuapp.com/" target="_blank" rel="noopener noreferrer">View Demo</a>
                     <a href="https://github.com/jimmyh3/team-i-chess" target="_blank" rel="noopener noreferrer">View Source Code</a>
-                </div>
-            </div>
-            <div className="project-card">
-                <img src={myplaceIcon} alt="Project"></img>
-                <div className="project-card-text">
-                    <h3>myPlace</h3>
-                    <p>
-                    myPlace is an apartment rental site tailored toward San Francisco State University students. 
-                    Students may register and apply to listings.
-                    </p>
-                    <ul className="project-card-skills">
-                        <li>PHP</li>
-                        <li>MySQL</li>
-                        <li>Bootstrap</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                    </ul>
-                </div>
-                <div className="project-card-links">
-                    <a href="https://myplace-sfsu.herokuapp.com/" target="_blank" rel="noopener noreferrer">View Demo</a>
-                    <a href="https://github.com/jimmyh3/myplace" target="_blank" rel="noopener noreferrer">View Source Code</a>                   
                 </div>
             </div>
         </div>
